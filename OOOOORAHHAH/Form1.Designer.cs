5e7f01974529d4867c860764515f11dab7f03c92
Gulliver Cardillo <0408428@schoolsnet.act.edu.au>
2026-04-22 11:26:39 +1000

    Add project files.

---
+++ OOOOORAHHAH/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace OOOOORAHHAH
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.POKIES = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(44, 132);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(225, 81);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "EMOJI";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // POKIES
+            // 
+            this.POKIES.AutoSize = true;
+            this.POKIES.Font = new System.Drawing.Font("Felix Titling", 24.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.POKIES.Location = new System.Drawing.Point(452, 198);
+            this.POKIES.Name = "POKIES";
+            this.POKIES.Size = new System.Drawing.Size(291, 39);
+            this.POKIES.TabIndex = 1;
+            this.POKIES.Text = "SLOT MACHINES";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(44, 252);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(225, 82);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "COLOUR";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label.Location = new System.Drawing.Point(583, 311);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(0, 31);
+            this.label.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.POKIES);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label POKIES;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label;
+    }
+}
+
---
+++ OOOOORAHHAH/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace OOOOORAHHAH
+{
+
+
+    public partial class Form1 : Form
+    {
+
+        List<string> emoji = new List<string>();
+        Random rand = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+            using (StreamReader sr = new StreamReader("C:/Users/0408428/Downloads/emoji.csv"))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    emoji.Add(line);
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+       
+            var value = rand.Next(emoji.Count());
+            POKIES.Text = emoji[value];
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int red = rand.Next(255);
+            int blue = rand.Next(255);
+            int green = rand.Next(255);
+
+
+
+            POKIES.ForeColor = Color.FromArgb(red, blue, green);
+
+
+        }
+
+    }
+}
