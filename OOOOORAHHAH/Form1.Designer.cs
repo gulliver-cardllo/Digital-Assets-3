16b8ead29e837a44dfeeb2a7a1dcccbcd6d91138
Gulliver Cardillo <0408428@schoolsnet.act.edu.au>
2026-04-22 11:58:44 +1000

    Added ui for  inventory db and menu sidebar

--- OOOOORAHHAH/Form1.Designer.cs
+++ OOOOORAHHAH/Form1.Designer.cs
@@ -28,41 +28,23 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.POKIES = new System.Windows.Forms.Label();
-            this.button2 = new System.Windows.Forms.Button();
             this.label = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnhome = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.itemname = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.qauntity = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Price = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(44, 132);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(225, 81);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "EMOJI";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // POKIES
-            // 
-            this.POKIES.AutoSize = true;
-            this.POKIES.Font = new System.Drawing.Font("Felix Titling", 24.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.POKIES.Location = new System.Drawing.Point(452, 198);
-            this.POKIES.Name = "POKIES";
-            this.POKIES.Size = new System.Drawing.Size(291, 39);
-            this.POKIES.TabIndex = 1;
-            this.POKIES.Text = "SLOT MACHINES";
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(44, 252);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(225, 82);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "COLOUR";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // label
             // 
@@ -73,29 +55,145 @@
             this.label.Size = new System.Drawing.Size(0, 31);
             this.label.TabIndex = 3;
             // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Gray;
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.btnhome);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(163, 450);
+            this.panel1.TabIndex = 4;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.panel2.Controls.Add(this.groupBox1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(163, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(637, 450);
+            this.panel2.TabIndex = 5;
+            // 
+            // btnhome
+            // 
+            this.btnhome.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnhome.Location = new System.Drawing.Point(0, 0);
+            this.btnhome.Name = "btnhome";
+            this.btnhome.Size = new System.Drawing.Size(163, 23);
+            this.btnhome.TabIndex = 0;
+            this.btnhome.Text = "button1";
+            this.btnhome.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button2.Location = new System.Drawing.Point(0, 23);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(163, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button3.Location = new System.Drawing.Point(0, 46);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(163, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.groupBox1.Controls.Add(this.dataGridView1);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(637, 450);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.itemname,
+            this.qauntity,
+            this.Price,
+            this.id});
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 16);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(631, 431);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // itemname
+            // 
+            this.itemname.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.itemname.HeaderText = "Item Name";
+            this.itemname.Name = "itemname";
+            this.itemname.ReadOnly = true;
+            // 
+            // qauntity
+            // 
+            this.qauntity.HeaderText = "Item Quantity";
+            this.qauntity.Name = "qauntity";
+            this.qauntity.ReadOnly = true;
+            // 
+            // Price
+            // 
+            this.Price.HeaderText = "Item Price";
+            this.Price.Name = "Price";
+            this.Price.ReadOnly = true;
+            // 
+            // id
+            // 
+            this.id.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.id.HeaderText = "Item ID";
+            this.id.MaxInputLength = 2147483647;
+            this.id.Name = "id";
+            this.id.ReadOnly = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.label);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.POKIES);
-            this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Label POKIES;
-        private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Label label;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnhome;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn itemname;
+        private System.Windows.Forms.DataGridViewTextBoxColumn qauntity;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Price;
+        private System.Windows.Forms.DataGridViewTextBoxColumn id;
     }
 }
 
--- OOOOORAHHAH/Form1.cs
+++ OOOOORAHHAH/Form1.cs
@@ -57,5 +57,24 @@ namespace OOOOORAHHAH
 
         }
 
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
     }
 }
