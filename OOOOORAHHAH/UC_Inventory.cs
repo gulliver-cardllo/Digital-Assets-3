41951b119160696c3f63ed9807eb51b7241cf5c6
Gulliver Cardillo <0408428@schoolsnet.act.edu.au>
2026-05-04 11:19:44 +1000

    adding testing shee thting csv

--- OOOOORAHHAH/UC_Inventory.cs
+++ OOOOORAHHAH/UC_Inventory.cs
@@ -6,15 +6,24 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 using System.Windows.Forms;
 
 namespace OOOOORAHHAH
 {
     public partial class UC_Inventory : UserControl
     {
+        List<String> invform = new List<string>();
         public UC_Inventory()
         {
             InitializeComponent();
+            using (StreamReader sr = new StreamReader("H:/Programming/OOOOORAHHAH/OOOOORAHHAH/test - Sheet1.csv"))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    invform.Add(line);
+                }
         }
 
         private void button3_Click(object sender, EventArgs e)
